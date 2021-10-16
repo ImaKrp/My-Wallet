e872a5fa4aaf99547d54b62968185b86ff420e7b
ImaKrp <julio.c04@aluno.ifsc.edu.br>
2021-10-16 12:45:12 -0300

    structure

---
+++ src/Pages/App/List/index.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export const List: React.FC = () => {
+    return (
+        <div>
+            
+        </div>
+    )
+}
---
+++ src/Pages/Sign/SignIn/index.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export const SignIn: React.FC = () => {
+    return (
+        <div>
+            
+        </div>
+    )
+}
